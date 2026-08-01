9398d2d9c342d7a9f1cad9f0b09d697e02a596c5
adityamottha <adityamottha5@gmail.com>
2026-08-01 17:36:26 +0530

    Completed AuthRepository

--- modules/repository/auth.repository.ts
+++ modules/repository/auth.repository.ts
@@ -1,11 +1,32 @@
 import AuthUser from "../../modules/models/auth.model";
 import { IUser } from "../types/auth.types";
-import { Schema } from "mongoose";
 
 export class AuthRepository{
   private model = AuthUser;
 
+  // FIND BY ID
   async findById(id: string): Promise<IUser | null> {
     return this.model.findById(id).exec();
+  };
+
+  // FIND BY EMAIL
+  async findByEmail(email:string):Promise<IUser | null>{
+    return this.model.findOne({email}).exec();
+  };
+
+  // FIND BY ROLE 
+  async findByRole(role:string):Promise<IUser | null >{
+    return this.model.findOne({role}).exec();
   }
+
+  // FIND BY PHONE-NUMBER
+   async findByPhoneNumber(phoneNumber:string):Promise<IUser | null>{
+    return this.model.findOne({phoneNumber}).exec();
+  };
+
+  // CREATE USER
+  async createUser(data: IUser):Promise<IUser | null>{
+    return this.model.findOne(data).exec();
+  };
+
 }
