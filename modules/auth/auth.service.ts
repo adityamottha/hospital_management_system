114615cfce9fb2975eccddab485a94b24d202524
adityamottha <adityamottha5@gmail.com>
2026-08-01 18:22:37 +0530

    Updated register service

--- modules/auth/auth.service.ts
+++ modules/auth/auth.service.ts
@@ -8,21 +8,7 @@ const authRepository = new AuthRepository();
 
 // REGISTER SERVICE =========================================
 export const registerService = async (data:RegisterInput):Promise<IUser>=>{
-    // check email is required
-    if(!data.email){
-        throw new ApiError(400,"Email-Address is required field")
-    };
-
-    // check password is required
-    if(!data.password){
-        throw new ApiError(400,"Password is required field")
-    };
-
-    // check password is required
-    if(!data.confirmPassword){
-        throw new ApiError(400,"confirmPassword is required field")
-    };
-
+    
     // check if user already registerd
     const existedUser = await authRepository.findByEmail(data.email);
 
