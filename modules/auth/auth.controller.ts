de33ad09766129751b11069a4915656e1bc2d3c2
adityamottha <adityamottha5@gmail.com>
2026-08-03 13:14:01 +0530

    Completed controller for register user

---
+++ modules/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { registerService } from "./auth.service";
+import { ApiResponse } from "../utils/apiResponse";
+import { NextResponse } from "next/server";
+
+export class AuthController{
+    static async registerController(req:Request){
+
+        // get body from json
+        const body = await req.json()
+
+        // call service function and pass body
+        const user = await registerService(body)
+
+        // return response
+        return NextResponse.json(
+            new  ApiResponse(200,user,"User registerd successfully!"),
+            { status:200 }
+        )
+    }
+}
