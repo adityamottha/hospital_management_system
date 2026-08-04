2d15d45d1641706e6159a79033e02bf031880491
adityamottha <adityamottha5@gmail.com>
2026-08-04 18:03:08 +0530

    SenVerificationEmail utils helper file completed

--- utils/sendVerificationEmail.ts
+++ utils/sendVerificationEmail.ts
@@ -1,2 +1,23 @@
 import { resend } from "@/modules/lib/resend";
 import VerificationEmail from "@/emails/verificationEmail";
+import { ApiError } from "./apiError";
+// import { ApiResponse } from "./apiResponse";
+
+// REGISTER VERIFICATION EMAIL
+export const  sendVerificationEmail = async(
+    email:string,
+    fullname:string,
+    verifyCode:string
+)=>{
+    try {
+  const verifyEmail = await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: [email],
+    subject: 'DR. AD-Clinic | Verification Code',
+    react: VerificationEmail({fullname,otp:verifyCode}),
+  });
+
+    } catch (error) {
+        throw new ApiError(500,`Error to sending verification email : ${error instanceof Error ? error.message:"Verification code error!"}`)
+    }
+}
