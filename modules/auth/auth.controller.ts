fa607cd831e1433462e34590f165c24f11d454c7
adityamottha <adityamottha5@gmail.com>
2026-08-06 14:02:28 +0530

    completed registration feature

--- modules/auth/auth.controller.ts
+++ modules/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { registerService } from "./auth.service";
 import { ApiResponse } from "../../utils/apiResponse";
 import { NextResponse } from "next/server";
+import { asyncHandler } from "@/utils/asyncHandler";
 
 export class AuthController{
-    static async registerController(req:Request){
-
-        // get body from json
+    static registerController(req:Request){
+        return asyncHandler(async ()=>{
+             // get body from json
         const body = await req.json()
 
         // call service function and pass body
@@ -16,5 +17,6 @@ export class AuthController{
             new  ApiResponse(200,user,"User registerd successfully!"),
             { status:200 }
         )
+        })
     }
 }
